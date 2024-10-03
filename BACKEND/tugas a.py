95babec5d7bd8b04951fe5925bdc99c512366e3c
alfinpepengshot <alfinpintar25@gmail.com>
2024-10-04 00:42:47 +0700

    Delete BACKEND directory

--- BACKEND/tugas a.py
+++
@@ -1,7 +0,0 @@
-# Script to check if total spending exceeds 100,000
-total_belanja = float(input("Masukkan total belanja: "))
-
-if total_belanja > 100000:
-    print("Selamat, Anda dapat hadiah!")
-else:
-    print("Terima kasih sudah berbelanja")
